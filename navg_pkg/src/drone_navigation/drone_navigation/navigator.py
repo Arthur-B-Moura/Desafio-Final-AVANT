e21579d69dc6c97ad08edcab13a791d966f75088
gabe-rbo <gabriel.ribeiro@dcc.ufmg.br>
2025-11-14 12:42:58 -0300

    More updates

--- navg_pkg/src/drone_navigation/drone_navigation/navigator.py
+++ navg_pkg/src/drone_navigation/drone_navigation/navigator.py
@@ -75,7 +75,7 @@ class NavigatorNode(Node):
         qos_be = QoSProfile(
             reliability=ReliabilityPolicy.BEST_EFFORT,
             history=HistoryPolicy.KEEP_LAST,
-            depth=1,
+            depth=10,
         )
 
         # Publishers
@@ -334,12 +334,12 @@ class NavigatorNode(Node):
         #    self._log_every(3.0, "Aguardando conexão com FCU e pose...")
         #    return
         self.get_logger().info('Aguardando conexão com o FCU...')
-        while rclpy.ok() and not getattr(self.fcu_state, "connected", False):
+        while not getattr(self.fcu_state, "connected", False):
             rclpy.spin_once(self, timeout_sec=5)
         self.get_logger().info('Conexão com FCU estabelecida.')
 
         self.get_logger().info('Aguardando leitura da posição...')
-        while rclpy.ok() and self.pose is None:
+        while self.pose is None:
             rclpy.spin_once(self, timeout_sec=5)
         self.get_logger().info(
             f'Posição inicial lida: {self.current_pose.x:.2f}, {self.current_pose.y:.2f}, {self.current_pose.z:.2f}')
